7f4e0b2253be4b2b87909ac4ce7ce5cf4748bda7
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-29 16:07:44 -0300

    utilizando o path variable

--- src/main/java/br/com/projeto/springbootapi2022/controle/Controle.java
+++ src/main/java/br/com/projeto/springbootapi2022/controle/Controle.java
@@ -1,13 +1,24 @@
 package br.com.projeto.springbootapi2022.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class Controle {
     @GetMapping("/")
-    public String mensagem  (){
+    public String mensagem() {
         return "olá mundo";
+    }
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "seja  bem  vindo(a) ";
+    }
+
+    @GetMapping("/boasVindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "seja  bem  vindo(a) " + nome;
     }
 }
