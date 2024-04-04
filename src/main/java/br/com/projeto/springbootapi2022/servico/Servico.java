039e74ba1ca99a7007a4f1da9a99bfdc9c4bb89b
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-04-04 10:05:20 -0300

    Annotation @Component

--- src/main/java/br/com/projeto/springbootapi2022/servico/Servico.java
+++ src/main/java/br/com/projeto/springbootapi2022/servico/Servico.java
@@ -1,8 +1,15 @@
 package br.com.projeto.springbootapi2022.servico;
 
+import br.com.projeto.springbootapi2022.modelo.mensagem.Mensagem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Servico {
+
+
+    @Autowired
+  private  Mensagem mensagem;
+
     
 }
